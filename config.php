e665b947e6910bb12c68f7aab2f1a3e83422036b
Kupo217 <nika.kupatadze.2@btu.edu.ge>
2022-06-28 15:02:48 +0400

    Created directories and basic configuration files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Access Denied!');
+
+const SITE_URL = 'http://my.web';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'web';
